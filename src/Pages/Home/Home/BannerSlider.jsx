fe21d405604c50c52dff731e21f87a771e46c28f
nahid-dev <nahidv232@gmail.com>
2023-05-19 02:45:52 +0600

    slider done

--- src/Pages/Home/Home/BannerSlider.jsx
+++ src/Pages/Home/Home/BannerSlider.jsx
@@ -8,30 +8,42 @@ const BannerSlider = () => {
     dots: true,
     fade: true,
     infinite: true,
+    autoplay: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
-    <div>
+    <>
       <div>
-        <h2>Fade</h2>
         <Slider {...settings}>
           <div>
-            <h2 className="text-3xl font-bold">Slide 1</h2>
+            <img
+              src="https://i.ibb.co/pRrttMw/hong-lin-Ze-YQxq-EUn-WQ-unsplash.jpg"
+              alt=""
+            />
           </div>
           <div>
-            <h2 className="text-3xl font-bold">Slide 2</h2>
+            <img
+              src="https://i.ibb.co/2nrhJw3/omar-albeik-I6g-WLzt-Kr-Yc-unsplash.jpg"
+              alt=""
+            />
           </div>
           <div>
-            <h2 className="text-3xl font-bold">Slide 3</h2>
+            <img
+              src="https://i.ibb.co/cxnp5nB/Background-1920x1280.png"
+              alt=""
+            />
           </div>
           <div>
-            <h2 className="text-3xl font-bold">Slide 4</h2>
+            <img
+              src="https://i.ibb.co/474jVxX/boukaih-p-RS6it-Ejhy-I-unsplash-1920x1280-200.jpg"
+              alt=""
+            />
           </div>
         </Slider>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
   return (
     <div>
       <BannerSlider></BannerSlider>
+      <div className="my-10">
+        <h2 className="font-bold text-5xl">Lorem ipsum dolor sit amet.</h2>
+      </div>
     </div>
   );
 };
